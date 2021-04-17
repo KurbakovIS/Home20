5ef54ae68dfbf3806ce82e58a8a655eb7224e0d1
Илья Курбаков <kurbakovila@gmail.com>
2021-04-17 11:27:48 +0300

    Реализованно удаление Поправленно чтение файла,

--- Home20/Controllers/AdminController.cs
+++ Home20/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Globalization;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Home20.Controllers
 {
@@ -20,8 +21,48 @@ namespace Home20.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Foods = new DataContext().Foods;
             return View();
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFood(int Id)
+        {
+            if(Id == 0)
+                return BadRequest();
+
+            try
+            {
+                using var db = new DataContext();
+                var findFood = await db.Foods.FindAsync(Id);
+                if (findFood != null)
+                {
+                    DeleteImg(findFood.Img);
+
+                    db.Foods.Remove(findFood);
+                    db.SaveChanges();
+                }
+                else
+                    return NotFound();
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest();
+            }
+
+          return  NoContent();
+        }
+
 
         /// <summary>
         /// Добавление позиции в меню
@@ -29,29 +70,32 @@ namespace Home20.Controllers
         /// <param name="food"></param>
         /// <returns></returns>
         [HttpPost]
-        public IActionResult CreateFood(CreateFood food)
+        public async Task<IActionResult> CreateFood(CreateFood food)
         {
 
             if (ModelState.IsValid)
             {
                 var uniqueNameFile = "";
+                var style = NumberStyles.Number | NumberStyles.AllowCurrencySymbol;
+                var provider = new CultureInfo("ru-RU");
+
                 if (food.Img != null)
                 {
-                    var uploads = Path.Combine(hostingEnvironment.WebRootPath, "img/uploads");
-                    uniqueNameFile = GetUniqueFileName(food.Img.FileName);
-                    var filePath = Path.Combine(uploads, uniqueNameFile);
+                    var uploads = Path.Combine(hostingEnvironment.WebRootPath, "img\\uploads");
+
                     if (!Directory.Exists(uploads))
                         Directory.CreateDirectory(uploads);
-                    food.Img.CopyTo(new FileStream(filePath, FileMode.Create));
 
+                    uniqueNameFile = GetUniqueFileName(food.Img.FileName);
+                    var filePath = Path.Combine(uploads, uniqueNameFile);
+                    FileStream neImg = new FileStream(filePath, FileMode.Create);
+                    await food.Img.CopyToAsync(neImg);
+                    neImg.Close();
                 }
 
                 try
                 {
-                    var style = NumberStyles.Number | NumberStyles.AllowCurrencySymbol;
-                    var provider = new CultureInfo("ru-RU");
                     var number = Decimal.Parse(food.Price.Replace('.',','), style, provider);
-                    Console.WriteLine("'{0}' converted to {1}.", food.Price, number);
 
                     using var db = new DataContext();
                     db.Foods.Add(
@@ -68,24 +112,42 @@ namespace Home20.Controllers
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
-                    return BadRequest();
+                    return BadRequest(e.Message);
                 }
                 return Ok();
             }
             else
-                return BadRequest();
+                return BadRequest("Модель не валидна");
         }
         /// <summary>
         /// Присваивает уникальное имя файлу
         /// </summary>
         /// <param name="FileName">Название файла</param>
         /// <returns>Уникальное названеифайла</returns>
-        private static string GetUniqueFileName(string FileName)
+        string GetUniqueFileName(string FileName)
         {
             FileName = Path.GetFileName(FileName);
             return Path.GetFileNameWithoutExtension(FileName)
                       + "_" + Guid.NewGuid().ToString().Substring(0, 4)
                       + Path.GetExtension(FileName);
+        }
+
+        /// <summary>
+        /// Удаляет картинку
+        /// </summary>
+        /// <param name="Img">название картинки</param>
+       async void DeleteImg(string Img)
+        {
+            var filePath = Path.Combine(hostingEnvironment.WebRootPath, Img);
+            FileInfo img = new FileInfo(filePath);
+            await  img.DeleteAsync();
+        }
+    }
+    public static class FileExtensions
+    {
+        public static Task DeleteAsync(this FileInfo fi)
+        {
+            return Task.Factory.StartNew(() => fi.Delete());
         }
     }
 }
